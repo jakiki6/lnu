7e68dfda5018b538ccde382fea54e0af4a1ca383
Jakob Kirsch <jakob.kirsch@web.de>
2023-04-03 21:50:18 +0200

    changes

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ set +h -e
 umask 022
 
 function download() {
-    if [ ! -f ../dist/$1 ]; then
-        wget $2 -O ../dist/$1
+    if [ ! -f dist/$1 ]; then
+        wget $2 -O dist/$1
     fi
 }
 
@@ -24,7 +24,7 @@ if [ ! -d dist ]; then mkdir dist; fi
 download busybox.tar.bz2 "https://www.busybox.net/downloads/busybox-1.35.0.tar.bz2"
 
 download gnupg.tar.bz2 "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.7.tar.bz2"
-download libgpg-error.tat.bz2 "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
+download libgpg-error.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
 download libgcrypt.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2"
 download libksba.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.6.1.tar.bz2"
 download libassuan.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2"
@@ -32,7 +32,7 @@ download npth.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2"
 
 download kexec-tools.tar.gz "https://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git/snapshot/kexec-tools-2.0.25.tar.gz"
 
-download linux.tar.gz "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz"
+download linux.tar.xz "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz"
 
 if [ ! -d toolchain ]; then
     echo "[*] building toolchain"
@@ -70,7 +70,7 @@ function compile_lib() {
     mkdir libs
     cd libs
 
-    tar vfx ../dist/$1 --strip-components=1
+    tar vfx ../../dist/$1 --strip-components=1
 
     CFLAGS="-Os" ./configure --prefix=$(pwd)/../deps --with-sysroot=$(pwd)/../../root --host=$TARGET --enable-shared=no --enable-static=yes  --with-libgpg-error-prefix=$(pwd)/../deps
     make -j$(nproc)
@@ -95,7 +95,7 @@ if [ ! -d gpg ]; then
 
     mkdir libs
 
-    tar vfx ../gpg.tar.bz2 --strip-components=1
+    tar vfx ../dist/gnupg.tar.bz2 --strip-components=1
 
     CFLAGS="-Os -fcommon" LDFLAGS="-static -s" ./configure --prefix=/ --with-sysroot=$(pwd)/../root --host=$TARGET \
         --with-npth-prefix=$(pwd)/deps \
@@ -133,7 +133,7 @@ if [ ! -d kexec ]; then
     mkdir kexec
     cd kexec
 
-    tar vfx ../dist/kexec.tar.gz --strip-components=1
+    tar vfx ../dist/kexec-tools.tar.gz --strip-components=1
 
     ./bootstrap
     CFLAGS="-Os -fcommon" LDFLAGS="-static -s" ./configure --prefix=/ --with-sysroot=$(pwd)/../root --host=$TARGET
