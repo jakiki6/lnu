7127b74454514fbdaa971624c8b80cd4603a0551
Jakob Kirsch <jakob.kirsch@web.de>
2023-04-03 14:03:33 +0200

    make downloads happen before build

--- build.sh
+++ build.sh
@@ -10,8 +10,29 @@ export LC_ALL=POSIX
 set +h -e
 umask 022
 
+function download() {
+    if [ ! -f ../dist/$1 ]; then
+        wget $2 -O ../dist/$1
+    fi
+}
+
 if [ ! -d build ]; then mkdir build; fi
 cd build
+
+if [ ! -d dist ]; then mkdir dist; fi
+
+download busybox.tar.bz2 "https://www.busybox.net/downloads/busybox-1.35.0.tar.bz2"
+
+download gnupg.tar.bz2 "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.7.tar.bz2"
+download libgpg-error.tat.bz2 "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
+download libgcrypt.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2"
+download libksba.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.6.1.tar.bz2"
+download libassuan.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2"
+download npth.tar.bz2 "https://www.gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2"
+
+download kexec-tools.tar.gz "https://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git/snapshot/kexec-tools-2.0.25.tar.gz"
+
+download linux.tar.gz "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz"
 
 if [ ! -d toolchain ]; then
     echo "[*] building toolchain"
@@ -37,8 +58,7 @@ if [ ! -d busybox ]; then
     mkdir busybox
     cd busybox
 
-    wget "https://www.busybox.net/downloads/busybox-1.35.0.tar.bz2" -O busybox.tar.bz2
-    tar vfx busybox.tar.bz2 --strip-components=1
+    tar vfx ../dist/busybox.tar.bz2 --strip-components=1
 
     cp ../../config/busybox .config
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE busybox install
@@ -50,8 +70,7 @@ function compile_lib() {
     mkdir libs
     cd libs
 
-    wget $1 -O lib.tar
-    tar vfx lib.tar --strip-components=1
+    tar vfx ../dist/$1 --strip-components=1
 
     CFLAGS="-Os" ./configure --prefix=$(pwd)/../deps --with-sysroot=$(pwd)/../../root --host=$TARGET --enable-shared=no --enable-static=yes  --with-libgpg-error-prefix=$(pwd)/../deps
     make -j$(nproc)
@@ -68,16 +87,15 @@ if [ ! -d gpg ]; then
     cd gpg
 
     mkdir deps
-    compile_lib "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
-    compile_lib "https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2"
-    compile_lib "https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.6.1.tar.bz2"
-    compile_lib "https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2"
-    compile_lib "https://www.gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2"
+    compile_lib libgpg-error.tar.bz2
+    compile_lib libgcrypt.tar.bz2
+    compile_lib libksba.tar.bz2
+    compile_lib libassuan.tar.bz2
+    compile_lib npth.tar.bz2
 
     mkdir libs
 
-    wget "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.7.tar.bz2" -O gpg.tar.bz2
-    tar vfx gpg.tar.bz2 --strip-components=1
+    tar vfx ../gpg.tar.bz2 --strip-components=1
 
     CFLAGS="-Os -fcommon" LDFLAGS="-static -s" ./configure --prefix=/ --with-sysroot=$(pwd)/../root --host=$TARGET \
         --with-npth-prefix=$(pwd)/deps \
@@ -115,8 +133,7 @@ if [ ! -d kexec ]; then
     mkdir kexec
     cd kexec
 
-    wget "https://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git/snapshot/kexec-tools-2.0.25.tar.gz" -O kexec.tar.gz
-    tar vfx kexec.tar.gz --strip-components=1
+    tar vfx ../dist/kexec.tar.gz --strip-components=1
 
     ./bootstrap
     CFLAGS="-Os -fcommon" LDFLAGS="-static -s" ./configure --prefix=/ --with-sysroot=$(pwd)/../root --host=$TARGET
@@ -138,8 +155,7 @@ if [ ! -d linux ]; then
     mkdir linux
     cd linux
 
-    wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz" -O linux.tar.xz
-    tar vfx linux.tar.xz --strip-components=1
+    tar vfx ../dist/linux.tar.xz --strip-components=1
 
     cp ../../config/linux.$ARCH .config
     make ARCH=$ARCH CROSS_COMPILE=$TARGET- -j$(nproc) bzImage
